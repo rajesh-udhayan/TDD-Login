38a8dbc84fdf7d80af6c31a2b5bd43b2b2244c9f
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-05-04 21:19:03 +0530

    Removed unused imports

--- app/src/main/java/com/rajesh/tddloginkata/LoginActivity.kt
+++ app/src/main/java/com/rajesh/tddloginkata/LoginActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
 import kotlinx.coroutines.launch
 
 class LoginActivity : ComponentActivity() {
