5d3ee847906550421860a398683eddf770d3bb9e
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-05-04 12:30:14 +0530

    Code refactor To remove the duplicated reference

--- app/src/test/java/com/rajesh/tddloginkata/LoginValidatorTest.kt
+++ app/src/test/java/com/rajesh/tddloginkata/LoginValidatorTest.kt
@@ -1,14 +1,20 @@
 package com.rajesh.tddloginkata
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class LoginValidatorTest {
 
+    private lateinit var validator: LoginValidator
+
+    @Before
+    fun setUp() {
+        validator = LoginValidator()
+    }
+
     @Test
     fun `should return empty credetntials when username and password are empty`() {
-        val validator = LoginValidator()
-
         val message = validator.validateCredentials("", "")
 
         assertThat(message).isEqualTo("Empty Credentials!")
@@ -16,8 +22,6 @@ class LoginValidatorTest {
 
     @Test
     fun `should return empty username when username is empty but password is not empty`() {
-        val validator = LoginValidator()
-
         val message = validator.validateCredentials("", "123")
 
         assertThat(message).isEqualTo("Empty Username!")
