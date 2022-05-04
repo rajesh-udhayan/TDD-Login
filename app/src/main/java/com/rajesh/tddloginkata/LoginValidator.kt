6a8aa76f74d1495e6bd99de3afed5c6f9cba504a
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-05-04 12:28:46 +0530

    Unit test passed for Empty username

--- app/src/main/java/com/rajesh/tddloginkata/LoginValidator.kt
+++ app/src/main/java/com/rajesh/tddloginkata/LoginValidator.kt
@@ -3,6 +3,9 @@ package com.rajesh.tddloginkata
 class LoginValidator {
 
     fun validateCredentials(username: String, password: String): String {
+        if (username.isEmpty() && !password.isEmpty()){
+            return "Empty Username!"
+        }
         return "Empty Credentials!"
     }
 
--- app/src/test/java/com/rajesh/tddloginkata/LoginValidatorTest.kt
+++ app/src/test/java/com/rajesh/tddloginkata/LoginValidatorTest.kt
@@ -6,11 +6,21 @@ import org.junit.Test
 class LoginValidatorTest {
 
     @Test
-    fun `should return empty credetntials when username and password are empty`(){
+    fun `should return empty credetntials when username and password are empty`() {
         val validator = LoginValidator()
 
-        val message = validator.validateCredentials("","")
+        val message = validator.validateCredentials("", "")
 
         assertThat(message).isEqualTo("Empty Credentials!")
+    }
+
+    @Test
+    fun `should return empty username when username is empty but password is not empty`() {
+        val validator = LoginValidator()
+
+        val message = validator.validateCredentials("", "123")
+
+        assertThat(message).isEqualTo("Empty Username!")
+
     }
 }
